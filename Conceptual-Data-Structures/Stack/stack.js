e19feec738596e9e7186575b3f5125b1d9fe9fcd
Junaid <156448521+junaid49-js@users.noreply.github.com>
2025-06-18 22:30:33 +0530

    Update stack.js
    Stack Implementation in JavaScript Using Class and Array.

--- Conceptual-Data-Structures/Stack/stack.js
+++ Conceptual-Data-Structures/Stack/stack.js
@@ -1,1 +1,47 @@
-
+class Stack {
+    constructor(capacity = Infinity) {
+      this.items = [];
+      this.capacity = capacity;
+    }
+  
+    push(element) {
+      if (this.isFull()) {
+        console.log("Overflow - Stack is full");
+        return "Overflow - Stack is full";
+      }
+      this.items.push(element);
+      console.log(`element ${element} added to stack`)
+    }
+  
+    pop() {
+      if (this.isEmpty()) {
+        console.log("Underflow - Stack is empty");
+        return "Underflow - Stack is empty";
+      }
+      return this.items.pop();
+    }
+  
+    peek() {
+      return this.items[this.items.length - 1];
+    }
+  
+    isEmpty() {
+      return this.items.length === 0;
+    }
+  
+    isFull() {
+      return this.items.length >= this.capacity;
+    }
+  
+    size() {
+      return this.items.length;
+    }
+  
+    print() {
+      console.log(this.items.toString());
+    }
+  
+    clear() {
+      this.items = [];
+    }
+  }
