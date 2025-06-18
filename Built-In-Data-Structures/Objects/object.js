529954e2f576b05ffaac96de9a127ebc9140e3e4
Junaid <156448521+junaid49-js@users.noreply.github.com>
2025-06-18 22:57:22 +0530

    Create object.js

---
+++ Built-In-Data-Structures/Objects/object.js
@@ -0,0 +1,2 @@
+// An object is an unordered collection of key-value pairs. The key must either be a string or symbol data type where as the value can be of any data types.
+// To retrieve a value, you can use the corresponding key This can be achieved using the dot notation or bracket notation
