5dc66c4fd3f2f6ab63c3a36d76cc05855eed5a4d
Junaid <156448521+junaid49-js@users.noreply.github.com>
2025-06-19 01:33:08 +0530

    Update object.js

--- Built-In-Data-Structures/Objects/object.js
+++ Built-In-Data-Structures/Objects/object.js
@@ -1,2 +1,8 @@
 // An object is an unordered collection of key-value pairs. The key must either be a string or symbol data type where as the value can be of any data types.
 // To retrieve a value, you can use the corresponding key This can be achieved using the dot notation or bracket notation
+// An object is not an iterable You cannot use it with a for of loop.
+
+const object = {
+  name: 'Bruce',
+  age: 25
+}
