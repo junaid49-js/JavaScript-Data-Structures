2026390a6def291b9fc293b7a2442349e474796a
Junaid <156448521+junaid49-js@users.noreply.github.com>
2025-06-18 19:46:15 +0530

    Arrays in JavaScript

--- Built-In-Data-Structures/Arrays/01-array.js
+++ Built-In-Data-Structures/Arrays/01-array.js
@@ -1,1 +1,28 @@
+// JavaScript array is an object that represents a collection of element.
+// Arrays can contain a mix of different Data Types. You can store strings, booleans, numbers or even objects all in the same array.
+// Arrays are resizable. You don't have to declare size of an array before creating it.
+// JavaScript arrays are zero-indexed and it maintains the insertion order.
+// Arrays are iterable, They can be used with a for of loop.
+// JavaScript arrays inherit from Array.prototype and have specific array-like behaviors and methods.
 
+// There are multiple ways to create arrays in JavaScript. Some of them are - 
+
+// 1. JavaScript Array Literal - 
+
+let arrayName1 = [value1, value2, value3, value4]
+// The values are contained inside [] and saperated by ,(Commas).
+
+// 2. JavaScript new Array Keyword -
+
+let arrayName2 = new Array();
+
+arrayName2[0] = value1;
+arrayName2[1] = value2;
+arrayName2[2] = value3;
+arrayName2[3] = value4;
+// Here we declare an initialize an empty array, and fill the array with accessing the array by indexing.
+
+// 3. JavaScript Array Constructur - 
+
+let arrayName3 = new Array(value1, value2, value3, value4);
+// This method is a mix of above two methods.
