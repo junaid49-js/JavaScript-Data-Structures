9714ff173815efe5f04a4e97eae7ab48addbf71b
Junaid <156448521+junaid49-js@users.noreply.github.com>
2025-06-18 21:22:46 +0530

    Update 02-accessing-elemets-in-array.js

--- Built-In-Data-Structures/Arrays/02-accessing-elemets-in-array.js
+++ Built-In-Data-Structures/Arrays/02-accessing-elemets-in-array.js
@@ -9,9 +9,24 @@ console.log("Original fruits array:", fruits); // Output - 'Original fruits arra
 // Here, the console.log() prints something to the console.
 // In this code, it prints a string "Original fruits array:" and then the array.
 
-// 2. Using indexes to update elements in the array. For this we will use the code structure like the JavaScript new Array Keyword when we assign elements
+// 2. Using indexes to update elements in the array. For this we will use the code structure like the JavaScript new Array Keyword when we assign elements.
 
-// To update an existing element, first access the array by the arrayName and use swuare brackets[] and add the index of the element you want to update. Then, assign the new value to the element
+// To update an existing element, first access the array by the arrayName and use square brackets[] and add the index of the element you want to update. Then, assign the new value to the element
 
 fruits[0] = "Mango"; // Change the first element
 console.log("fruits after modification:", fruits); // Output: 'fruits after modification: ["Mango", "Banana", "Cherry", "Date"]'
+
+// 3. Using indexes to create and add elements to the array. For this we will use the code structure like the JavaScript new Array Keyword when we assign elements.
+
+/* To create and add a new element to the array, first access the array by the arrayName and use square brackets[] and add the index of 
+where you want to create and add new element. Then, assign the new value to it, The indices where no value is placed will be an empty item */
+
+
+fruits[6] = 'Pineapple' // Adds new element to index 6
+console.log("fruits after adding value to index 6:", fruits) // Output: 'fruits after adding value to index 6: [ 'Mango', 'Banana', 'Cherry', 'Date', <2 empty items>, 'Pineapple' ]'
+
+fruits[4] = 'Apple' // Adds new element to index 4
+console.log("fruits after adding value to index 4:", fruits) // Output: 'fruits after adding value to index 6: [ 'Mango', 'Banana', 'Cherry', 'Date', 'Apple', <1 empty items>, 'Pineapple' ]'
+
+fruits[5] = 'Strawberry' // Adds new element to index 5
+console.log("fruits after adding value to index 5:", fruits) // Output: 'fruits after adding value to index 6: [ 'Mango', 'Banana', 'Cherry', 'Date', 'Apple', 'Strawberry', 'Pineapple' ]'
