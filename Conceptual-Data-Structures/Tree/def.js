f467bd77892c342f78204d3602cf4464ca748752
Junaid <156448521+junaid49-js@users.noreply.github.com>
2025-06-27 22:39:43 +0530

    Create def.js

---
+++ Conceptual-Data-Structures/Tree/def.js
@@ -0,0 +1,5 @@
+// A Tree is a hierarchical data structure that consists of nodes connected by edges.
+// A Tree is a non linear data structure, compared to arrays,linked lists, stacks and queues which are linear data structures.
+// In linear data structure, the time require to search is proportional to the size of the data set.
+// Trees however, owing to the nonlinear nature allows quicker and easier search access to the data.
+// A tree will not contain any loops or cycles.
