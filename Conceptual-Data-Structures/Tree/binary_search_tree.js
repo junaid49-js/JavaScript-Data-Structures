fa1670c62df9a832e11153440abf0179484cd9fb
Junaid <156448521+junaid49-js@users.noreply.github.com>
2025-06-28 01:38:48 +0530

    Update binary_search_tree.js
    Added Insertion to the Binary Search Tree.

--- Conceptual-Data-Structures/Tree/binary_search_tree.js
+++ Conceptual-Data-Structures/Tree/binary_search_tree.js
@@ -14,4 +14,30 @@ class binarySearchTree{
   isEmpty(){
     return this.root === null;
   }
+
+  insert(value){
+    let newNode = new Node(value);
+    if(isEmpty()){
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+    
+  }
+
+  insertNode(root, newNode){
+    if(newNode.value < root.value){
+      if(root.leftChild === null){
+        root.leftChild = newNode;
+      } else {
+        this.insertNode(root.leftChild, newNode)
+      }
+    } else {
+      if(root.rightChild === null){
+        root.rightChild = newNode;
+      } else {
+        this.insertNode(root.rightChild, newNode)
+      }
+    }
+  }
 }
